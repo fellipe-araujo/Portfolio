829d8a9b8e7b011142b5146eec8462a4bf9b0693
Fellipe <araujo.fellipe@hotmail.com>
2021-06-30 20:02:15 -0300

    Add projects data.

---
+++ web/src/database/projects.ts
@@ -0,0 +1,132 @@
+export const projects = {
+  web: [
+    {
+      id: 1,
+      title: 'Letmeask',
+      coverImage: 'letmeask-cover',
+      images: [],
+      stacks: [
+        { id: 1, title: 'ReactJS' },
+        { id: 2, title: 'Firebase' },
+        { id: 3, title: 'TypeScript' },
+      ],
+      description:
+        'Letmeask é perfeito para criadores de conteúdos poderem criar salas de Q&A com o seu público, de uma forma muito organizada e democrática.',
+    },
+    {
+      id: 2,
+      title: 'Happy',
+      coverImage: 'happy-cover',
+      images: [],
+      stacks: [
+        { id: 1, title: 'React.js' },
+        { id: 2, title: 'React Native' },
+        { id: 3, title: 'Expo' },
+        { id: 4, title: 'Node.js' },
+        { id: 5, title: 'TypeScript' },
+      ],
+      description:
+        'O Happy tem o intuito de cadastrar Orfanatos informando dados como, por exemplo, nome, foto(s), instruções, descrição, horário de atendimento e se funciona nos fins de semana.',
+    },
+    {
+      id: 3,
+      title: 'Ecoleta',
+      coverImage: 'ecoleta-cover',
+      images: [],
+      stacks: [
+        { id: 1, title: 'React.js' },
+        { id: 2, title: 'React Native' },
+        { id: 3, title: 'Expo' },
+        { id: 4, title: 'Node.js' },
+      ],
+      description:
+        'O Ecoleta é um marketplace que ajuda pessoas a encontrarem pontos de coleta de resíduos de forma eficiente.',
+    },
+    {
+      id: 4,
+      title: 'BeTheHero',
+      coverImage: 'bethehero-cover',
+      images: [],
+      stacks: [
+        { id: 1, title: 'React.js' },
+        { id: 2, title: 'React Native' },
+        { id: 3, title: 'Expo' },
+        { id: 4, title: 'Node.js' },
+      ],
+      description:
+        'O BeTheHero tem o intuito de ONGs cadastrarem a si próprias e seus respectivos casos na versão Web. Na versão Mobile, as pessoas podem procurar por qual caso elas querem ajudar fazendo uma doação.',
+    },
+    {
+      id: 5,
+      title: 'BigGameSurvey',
+      coverImage: 'biggamesurvey-cover',
+      images: [],
+      stacks: [
+        { id: 1, title: 'React.js' },
+        { id: 2, title: 'React Native' },
+        { id: 3, title: 'Expo' },
+        { id: 4, title: 'Java' },
+        { id: 5, title: 'TypeScript' },
+      ],
+      description:
+        'O BigGameSurvey tem o intuito de coletar a opinião dos jogadores sobre qual o jogo que eles mais gostam, informando o nome do jogo, gênero e plataforma (PlayStation, XBOX ou PC). É mostrado um resumo em gráficos dos jogos, gêneros e plataformas mais jogadas, além dos dados de quem participou da pesquisa.',
+    },
+    {
+      id: 6,
+      title: 'Delivery',
+      coverImage: 'delivery-cover',
+      images: [],
+      stacks: [
+        { id: 1, title: 'React.js' },
+        { id: 2, title: 'React Native' },
+        { id: 3, title: 'Expo' },
+        { id: 4, title: 'Java' },
+        { id: 5, title: 'TypeScript' },
+      ],
+      description:
+        'Delivery é baseado em uma entrega de pedidos, no qual na versão Web uma pessoa pode escolher a sua comida, definir e concluir seu pedido. Já a versão Mobile, ela é utilizada pelo entregador, o qual pode ver todos os pedidos pendentes, escolher um e iniciar a navegação (integrado com o Google Maps para definir a rota de entrega).',
+    },
+  ],
+  mobile: [
+    {
+      id: 1,
+      title: 'Happy',
+      coverImage: '',
+      images: [],
+      stacks: [],
+      description: '',
+    },
+    {
+      id: 2,
+      title: 'Ecoleta',
+      coverImage: '',
+      images: [],
+      stacks: [],
+      description: '',
+    },
+    {
+      id: 3,
+      title: 'BeTheHero',
+      coverImage: '',
+      images: [],
+      stacks: [],
+      description: '',
+    },
+    {
+      id: 4,
+      title: 'BigGameSurvey',
+      coverImage: '',
+      images: [],
+      stacks: [],
+      description: '',
+    },
+    {
+      id: 5,
+      title: 'Delivery',
+      coverImage: '',
+      images: [],
+      stacks: [],
+      description: '',
+    },
+  ],
+};
