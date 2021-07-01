4c583a4d7d5ee86b8dd33f0db10a7bf9a7011aed
Fellipe <araujo.fellipe@hotmail.com>
2021-07-01 16:49:51 -0300

    Update projects database.

--- web/src/database/projects.ts
+++ web/src/database/projects.ts
@@ -6,12 +6,13 @@ export const projects = {
       coverImage: 'letmeask-cover',
       images: [],
       stacks: [
-        { id: 1, title: 'ReactJS' },
-        { id: 2, title: 'Firebase' },
-        { id: 3, title: 'TypeScript' },
+        { id: 1, title: 'React.js', color: '#61DAFB' },
+        { id: 2, title: 'Firebase', color: '#F7C52B' },
+        { id: 3, title: 'TypeScript', color: '#3178C6' },
       ],
       description:
         'Letmeask é perfeito para criadores de conteúdos poderem criar salas de Q&A com o seu público, de uma forma muito organizada e democrática.',
+      platform: ['web'],
     },
     {
       id: 2,
@@ -19,14 +20,15 @@ export const projects = {
       coverImage: 'happy-cover',
       images: [],
       stacks: [
-        { id: 1, title: 'React.js' },
-        { id: 2, title: 'React Native' },
-        { id: 3, title: 'Expo' },
-        { id: 4, title: 'Node.js' },
-        { id: 5, title: 'TypeScript' },
+        { id: 1, title: 'React.js', color: '#61DAFB' },
+        { id: 2, title: 'React Native', color: '#9B79FC' },
+        { id: 3, title: 'Expo', color: '#fff' },
+        { id: 4, title: 'Node.js', color: '#51A15E' },
+        { id: 5, title: 'TypeScript', color: '#3178C6' },
       ],
       description:
         'O Happy tem o intuito de cadastrar Orfanatos informando dados como, por exemplo, nome, foto(s), instruções, descrição, horário de atendimento e se funciona nos fins de semana.',
+      platform: ['web', 'mobile'],
     },
     {
       id: 3,
@@ -34,13 +36,14 @@ export const projects = {
       coverImage: 'ecoleta-cover',
       images: [],
       stacks: [
-        { id: 1, title: 'React.js' },
-        { id: 2, title: 'React Native' },
-        { id: 3, title: 'Expo' },
-        { id: 4, title: 'Node.js' },
+        { id: 1, title: 'React.js', color: '#61DAFB' },
+        { id: 2, title: 'React Native', color: '#9B79FC' },
+        { id: 3, title: 'Expo', color: '#fff' },
+        { id: 4, title: 'Node.js', color: '#51A15E' },
       ],
       description:
         'O Ecoleta é um marketplace que ajuda pessoas a encontrarem pontos de coleta de resíduos de forma eficiente.',
+      platform: ['web', 'mobile'],
     },
     {
       id: 4,
@@ -48,13 +51,14 @@ export const projects = {
       coverImage: 'bethehero-cover',
       images: [],
       stacks: [
-        { id: 1, title: 'React.js' },
-        { id: 2, title: 'React Native' },
-        { id: 3, title: 'Expo' },
-        { id: 4, title: 'Node.js' },
+        { id: 1, title: 'React.js', color: '#61DAFB' },
+        { id: 2, title: 'React Native', color: '#9B79FC' },
+        { id: 3, title: 'Expo', color: '#fff' },
+        { id: 4, title: 'Node.js', color: '#51A15E' },
       ],
       description:
         'O BeTheHero tem o intuito de ONGs cadastrarem a si próprias e seus respectivos casos na versão Web. Na versão Mobile, as pessoas podem procurar por qual caso elas querem ajudar fazendo uma doação.',
+      platform: ['web', 'mobile'],
     },
     {
       id: 5,
@@ -62,14 +66,15 @@ export const projects = {
       coverImage: 'biggamesurvey-cover',
       images: [],
       stacks: [
-        { id: 1, title: 'React.js' },
-        { id: 2, title: 'React Native' },
-        { id: 3, title: 'Expo' },
-        { id: 4, title: 'Java' },
-        { id: 5, title: 'TypeScript' },
+        { id: 1, title: 'React.js', color: '#61DAFB' },
+        { id: 2, title: 'React Native', color: '#9B79FC' },
+        { id: 3, title: 'Expo', color: '#fff' },
+        { id: 4, title: 'Java', color: '#ED2025' },
+        { id: 5, title: 'TypeScript', color: '#3178C6' },
       ],
       description:
         'O BigGameSurvey tem o intuito de coletar a opinião dos jogadores sobre qual o jogo que eles mais gostam, informando o nome do jogo, gênero e plataforma (PlayStation, XBOX ou PC). É mostrado um resumo em gráficos dos jogos, gêneros e plataformas mais jogadas, além dos dados de quem participou da pesquisa.',
+      platform: ['web', 'mobile'],
     },
     {
       id: 6,
@@ -77,14 +82,15 @@ export const projects = {
       coverImage: 'delivery-cover',
       images: [],
       stacks: [
-        { id: 1, title: 'React.js' },
-        { id: 2, title: 'React Native' },
-        { id: 3, title: 'Expo' },
-        { id: 4, title: 'Java' },
-        { id: 5, title: 'TypeScript' },
+        { id: 1, title: 'React.js', color: '#61DAFB' },
+        { id: 2, title: 'React Native', color: '#9B79FC' },
+        { id: 3, title: 'Expo', color: '#fff' },
+        { id: 4, title: 'Java', color: '#ED2025' },
+        { id: 5, title: 'TypeScript', color: '#3178C6' },
       ],
       description:
         'Delivery é baseado em uma entrega de pedidos, no qual na versão Web uma pessoa pode escolher a sua comida, definir e concluir seu pedido. Já a versão Mobile, ela é utilizada pelo entregador, o qual pode ver todos os pedidos pendentes, escolher um e iniciar a navegação (integrado com o Google Maps para definir a rota de entrega).',
+      platform: ['web', 'mobile'],
     },
   ],
   mobile: [
