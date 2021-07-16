78b42f92e445c17c979b10ea605de6ccfa5cb231
Fellipe <araujo.fellipe@hotmail.com>
2021-07-16 11:03:38 -0300

    Update projects database.

--- web/src/database/projects.ts
+++ web/src/database/projects.ts
@@ -1,142 +1,202 @@
-export const projects = {
-  web: [
-    {
-      id: 1,
-      title: 'Letmeask',
-      coverImage: 'letmeask-cover',
-      images: [],
-      stacks: [
-        { id: 1, title: 'React.js', color: '#61DAFB' },
-        { id: 2, title: 'Firebase', color: '#F7C52B' },
-        { id: 3, title: 'TypeScript', color: '#3178C6' },
+export const projects = [
+  {
+    id: 1,
+    title: 'Letmeask',
+    coverImage: 'letmeask-cover',
+    images: {
+      web: [
+        { id: 1, pathName: 'web-1' },
+        { id: 2, pathName: 'web-2' },
+        { id: 3, pathName: 'web-3' },
+        { id: 4, pathName: 'web-4' },
+        { id: 5, pathName: 'web-5' },
+        { id: 6, pathName: 'web-6' },
+        { id: 7, pathName: 'web-7' },
+        { id: 8, pathName: 'web-8' },
+        { id: 9, pathName: 'web-9' },
+        { id: 10, pathName: 'web-10' },
+        { id: 11, pathName: 'web-11' },
       ],
-      description:
-        'Letmeask é perfeito para criadores de conteúdos poderem criar salas de Q&A com o seu público, de uma forma muito organizada e democrática.',
-      platform: ['web'],
+      mobile: [],
     },
-    {
-      id: 2,
-      title: 'Happy',
-      coverImage: 'happy-cover',
-      images: [],
-      stacks: [
-        { id: 1, title: 'React.js', color: '#61DAFB' },
-        { id: 2, title: 'React Native', color: '#9B79FC' },
-        { id: 3, title: 'Expo', color: '#fff' },
-        { id: 4, title: 'Node.js', color: '#51A15E' },
-        { id: 5, title: 'TypeScript', color: '#3178C6' },
+    stacks: [
+      { id: 1, title: 'React.js', color: '#61DAFB' },
+      { id: 2, title: 'Firebase', color: '#F7C52B' },
+      { id: 3, title: 'TypeScript', color: '#3178C6' },
+    ],
+    description:
+      'Letmeask é perfeito para criadores de conteúdos poderem criar salas de Q&A com o seu público, de uma forma muito organizada e democrática.',
+    platform: [{ id: 1, title: 'web' }],
+    figma: 'https://www.figma.com/file/0oOjHq2HUxgBPXN7QiHaMI/Letmeask',
+    github: 'https://github.com/fellipe-araujo/Letmeask',
+  },
+  {
+    id: 2,
+    title: 'Happy',
+    coverImage: 'happy-cover',
+    images: {
+      web: [
+        { id: 1, pathName: 'web-1' },
+        { id: 2, pathName: 'web-2' },
+        { id: 3, pathName: 'web-3' },
+        { id: 4, pathName: 'web-4' },
+        { id: 5, pathName: 'web-5' },
+        { id: 6, pathName: 'web-6' },
       ],
-      description:
-        'O Happy tem o intuito de cadastrar Orfanatos informando dados como, por exemplo, nome, foto(s), instruções, descrição, horário de atendimento e se funciona nos fins de semana.',
-      platform: ['web', 'mobile'],
-    },
-    {
-      id: 3,
-      title: 'Ecoleta',
-      coverImage: 'ecoleta-cover',
-      images: [],
-      stacks: [
-        { id: 1, title: 'React.js', color: '#61DAFB' },
-        { id: 2, title: 'React Native', color: '#9B79FC' },
-        { id: 3, title: 'Expo', color: '#fff' },
-        { id: 4, title: 'Node.js', color: '#51A15E' },
+      mobile: [
+        { id: 1, pathName: 'mobile-1' },
+        { id: 2, pathName: 'mobile-2' },
+        { id: 3, pathName: 'mobile-3' },
+        { id: 4, pathName: 'mobile-4' },
+        { id: 5, pathName: 'mobile-5' },
+        { id: 6, pathName: 'mobile-6' },
+        { id: 7, pathName: 'mobile-7' },
+        { id: 8, pathName: 'mobile-8' },
       ],
-      description:
-        'O Ecoleta é um marketplace que ajuda pessoas a encontrarem pontos de coleta de resíduos de forma eficiente.',
-      platform: ['web', 'mobile'],
     },
-    {
-      id: 4,
-      title: 'BeTheHero',
-      coverImage: 'bethehero-cover',
-      images: [],
-      stacks: [
-        { id: 1, title: 'React.js', color: '#61DAFB' },
-        { id: 2, title: 'React Native', color: '#9B79FC' },
-        { id: 3, title: 'Expo', color: '#fff' },
-        { id: 4, title: 'Node.js', color: '#51A15E' },
+    stacks: [
+      { id: 1, title: 'React.js', color: '#61DAFB' },
+      { id: 2, title: 'React Native', color: '#9B79FC' },
+      { id: 3, title: 'Expo', color: '#fff' },
+      { id: 4, title: 'Node.js', color: '#51A15E' },
+      { id: 5, title: 'TypeScript', color: '#3178C6' },
+    ],
+    description:
+      'O Happy tem o intuito de cadastrar Orfanatos informando dados como, por exemplo, nome, foto(s), instruções, descrição, horário de atendimento e se funciona nos fins de semana.',
+    platform: [
+      { id: 1, title: 'web' },
+      { id: 2, title: 'mobile' },
+    ],
+    figma: 'https://www.figma.com/file/CN36uxQwONb09B1a4kcp14/Happy-Web',
+    github: 'https://github.com/fellipe-araujo/Happy',
+  },
+  {
+    id: 3,
+    title: 'Ecoleta',
+    coverImage: 'ecoleta-cover',
+    images: {
+      web: [
+        { id: 1, pathName: 'web-1' },
+        { id: 2, pathName: 'web-2' },
+        { id: 3, pathName: 'web-3' },
+        { id: 4, pathName: 'web-4' },
       ],
-      description:
-        'O BeTheHero tem o intuito de ONGs cadastrarem a si próprias e seus respectivos casos na versão Web. Na versão Mobile, as pessoas podem procurar por qual caso elas querem ajudar fazendo uma doação.',
-      platform: ['web', 'mobile'],
-    },
-    {
-      id: 5,
-      title: 'BigGameSurvey',
-      coverImage: 'biggamesurvey-cover',
-      images: [],
-      stacks: [
-        { id: 1, title: 'React.js', color: '#61DAFB' },
-        { id: 2, title: 'React Native', color: '#9B79FC' },
-        { id: 3, title: 'Expo', color: '#fff' },
-        { id: 4, title: 'Java', color: '#ED2025' },
-        { id: 5, title: 'TypeScript', color: '#3178C6' },
+      mobile: [
+        { id: 1, pathName: 'mobile-1' },
+        { id: 2, pathName: 'mobile-2' },
+        { id: 3, pathName: 'mobile-3' },
+        { id: 4, pathName: 'mobile-4' },
       ],
-      description:
-        'O BigGameSurvey tem o intuito de coletar a opinião dos jogadores sobre qual o jogo que eles mais gostam, informando o nome do jogo, gênero e plataforma (PlayStation, XBOX ou PC). É mostrado um resumo em gráficos dos jogos, gêneros e plataformas mais jogadas, além dos dados de quem participou da pesquisa.',
-      platform: ['web', 'mobile'],
     },
-    {
-      id: 6,
-      title: 'Delivery',
-      coverImage: 'delivery-cover',
-      images: [
-        { id: 1, pathName: 'web-1.png' },
-        { id: 2, pathName: 'web-2.png' },
-        { id: 3, pathName: 'web-3.png' },
+    stacks: [
+      { id: 1, title: 'React.js', color: '#61DAFB' },
+      { id: 2, title: 'React Native', color: '#9B79FC' },
+      { id: 3, title: 'Expo', color: '#fff' },
+      { id: 4, title: 'Node.js', color: '#51A15E' },
+    ],
+    description:
+      'O Ecoleta é um marketplace que ajuda pessoas a encontrarem pontos de coleta de resíduos de forma eficiente.',
+    platform: [
+      { id: 1, title: 'web' },
+      { id: 2, title: 'mobile' },
+    ],
+    figma: 'https://www.figma.com/file/7bppDHWvofhCxpJ0aE13X9/Ecoleta',
+    github: 'https://github.com/fellipe-araujo/Ecoleta',
+  },
+  {
+    id: 4,
+    title: 'BeTheHero',
+    coverImage: 'bethehero-cover',
+    images: {
+      web: [
+        { id: 1, pathName: 'web-1' },
+        { id: 2, pathName: 'web-2' },
+        { id: 3, pathName: 'web-3' },
+        { id: 4, pathName: 'web-4' },
       ],
-      stacks: [
-        { id: 1, title: 'React.js', color: '#61DAFB' },
-        { id: 2, title: 'React Native', color: '#9B79FC' },
-        { id: 3, title: 'Expo', color: '#fff' },
-        { id: 4, title: 'Java', color: '#ED2025' },
-        { id: 5, title: 'TypeScript', color: '#3178C6' },
+      mobile: [
+        { id: 1, pathName: 'mobile-1' },
+        { id: 2, pathName: 'mobile-2' },
+        { id: 3, pathName: 'mobile-3' },
       ],
-      description:
-        'Delivery é baseado em uma entrega de pedidos, no qual na versão Web uma pessoa pode escolher a sua comida, definir e concluir seu pedido. Já a versão Mobile, ela é utilizada pelo entregador, o qual pode ver todos os pedidos pendentes, escolher um e iniciar a navegação (integrado com o Google Maps para definir a rota de entrega).',
-      platform: ['web', 'mobile'],
     },
-  ],
-  mobile: [
-    {
-      id: 1,
-      title: 'Happy',
-      coverImage: '',
-      images: [],
-      stacks: [],
-      description: '',
+    stacks: [
+      { id: 1, title: 'React.js', color: '#61DAFB' },
+      { id: 2, title: 'React Native', color: '#9B79FC' },
+      { id: 3, title: 'Expo', color: '#fff' },
+      { id: 4, title: 'Node.js', color: '#51A15E' },
+    ],
+    description:
+      'O BeTheHero tem o intuito de ONGs cadastrarem a si próprias e seus respectivos casos na versão Web. Na versão Mobile, as pessoas podem procurar por qual caso elas querem ajudar fazendo uma doação.',
+    platform: [
+      { id: 1, title: 'web' },
+      { id: 2, title: 'mobile' },
+    ],
+    figma: 'https://www.figma.com/file/GbgduoZHjeKqk2qRNfAUaO/Be-The-Hero',
+    github: 'https://github.com/fellipe-araujo/BeTheHero',
+  },
+  {
+    id: 5,
+    title: 'BigGameSurvey',
+    coverImage: 'biggamesurvey-cover',
+    images: {
+      web: [
+        { id: 1, pathName: 'web-1' },
+        { id: 2, pathName: 'web-2' },
+        { id: 3, pathName: 'web-3' },
+      ],
+      mobile: [
+        { id: 1, pathName: 'mobile-1' },
+        { id: 2, pathName: 'mobile-2' },
+        { id: 3, pathName: 'mobile-3' },
+      ],
     },
-    {
-      id: 2,
-      title: 'Ecoleta',
-      coverImage: '',
-      images: [],
-      stacks: [],
-      description: '',
+    stacks: [
+      { id: 1, title: 'React.js', color: '#61DAFB' },
+      { id: 2, title: 'React Native', color: '#9B79FC' },
+      { id: 3, title: 'Expo', color: '#fff' },
+      { id: 4, title: 'Java', color: '#ED2025' },
+      { id: 5, title: 'TypeScript', color: '#3178C6' },
+    ],
+    description:
+      'O BigGameSurvey tem o intuito de coletar a opinião dos jogadores sobre qual o jogo que eles mais gostam, informando o nome do jogo, gênero e plataforma (PlayStation, XBOX ou PC). É mostrado um resumo em gráficos dos jogos, gêneros e plataformas mais jogadas, além dos dados de quem participou da pesquisa.',
+    platform: [
+      { id: 1, title: 'web' },
+      { id: 2, title: 'mobile' },
+    ],
+    figma: 'https://www.figma.com/file/jG9ZWzOCA1xius5LyLitME/BigGameSurvey',
+    github: 'https://github.com/fellipe-araujo/BigGameSurvey',
+  },
+  {
+    id: 6,
+    title: 'Delivery',
+    coverImage: 'delivery-cover',
+    images: {
+      web: [
+        { id: 1, pathName: 'web-1' },
+        { id: 2, pathName: 'web-2' },
+        { id: 3, pathName: 'web-3' },
+      ],
+      mobile: [
+        { id: 1, pathName: 'mobile-1' },
+        { id: 2, pathName: 'mobile-2' },
+        { id: 3, pathName: 'mobile-3' },
+      ],
     },
-    {
-      id: 3,
-      title: 'BeTheHero',
-      coverImage: '',
-      images: [],
-      stacks: [],
-      description: '',
-    },
-    {
-      id: 4,
-      title: 'BigGameSurvey',
-      coverImage: '',
-      images: [],
-      stacks: [],
-      description: '',
-    },
-    {
-      id: 5,
-      title: 'Delivery',
-      coverImage: '',
-      images: [],
-      stacks: [],
-      description: '',
-    },
-  ],
-};
+    stacks: [
+      { id: 1, title: 'React.js', color: '#61DAFB' },
+      { id: 2, title: 'React Native', color: '#9B79FC' },
+      { id: 3, title: 'Expo', color: '#fff' },
+      { id: 4, title: 'Java', color: '#ED2025' },
+      { id: 5, title: 'TypeScript', color: '#3178C6' },
+    ],
+    description:
+      'Delivery é baseado em uma entrega de pedidos, no qual na versão Web uma pessoa pode escolher a sua comida, definir e concluir seu pedido. Já a versão Mobile, ela é utilizada pelo entregador, o qual pode ver todos os pedidos pendentes, escolher um e iniciar a navegação (integrado com o Google Maps para definir a rota de entrega).',
+    platform: [
+      { id: 1, title: 'web' },
+      { id: 2, title: 'mobile' },
+    ],
+    figma: 'https://www.figma.com/file/vdRIG697gVQnKiWCLJNDZp/Delivery',
+    github: 'https://github.com/fellipe-araujo/Delivery',
+  },
+];
