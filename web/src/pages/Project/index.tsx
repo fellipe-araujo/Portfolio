69f9e14bd5f5b2df7eacfae9452b72392e0d6d63
Fellipe <araujo.fellipe@hotmail.com>
2021-07-18 11:39:59 -0300

    Develop Project page and add media queries.

--- web/src/pages/Project/index.tsx
+++ web/src/pages/Project/index.tsx
@@ -1,7 +1,13 @@
-import { ProjectContainer } from './styles';
+import { useState, useEffect } from 'react';
+import { ProjectContainer, Platform, Carousel } from './styles';
 import { useParams } from 'react-router-dom';
 import Slider from 'react-slick';
+import Footer from '../../components/Footer';
+import StackTag from '../../components/StackTag';
+import Separator from '../../components/Separator';
 import { projects } from '../../database/projects';
+import { settingsWeb, settingsMobile } from '../../utils/CarouselConfig';
+import { ProjectProps } from '../../utils/types';
 
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
@@ -11,33 +17,115 @@ interface ParamsProps {
 }
 
 const Project = () => {
+  const [project, setProject] = useState<ProjectProps>();
+
   const params: ParamsProps = useParams();
 
-  const settings = {
-    dots: true,
-    // centerMode: true,
-    // centerPadding: '0px',
-    infinite: false,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  };
+  useEffect(() => {
+    const loadingProject = () => {
+      setProject(projects[parseInt(params.id) - 1]);
+    };
+
+    loadingProject();
+  }, [params.id]);
 
   return (
     <ProjectContainer>
-      <div className="project-carousel-container">
-        <Slider {...settings}>
-          {projects.web[parseInt(params.id) - 1].images.map((image) => (
-            <div key={image.id} className="project-carousel-content">
-              <img
-                className="project-carousel-image"
-                src={`/projects/Delivery/${image.pathName}`}
-                alt={image.pathName}
-              />
-            </div>
-          ))}
-        </Slider>
+      <h1 className="project-title">{project?.title}</h1>
+
+      <Separator />
+
+      {projects[parseInt(params.id) - 1].platform.some(
+        (item) => item.title === 'web'
+      ) ? (
+        <>
+          <Platform web>
+            <h2 className="project-platform-title">
+              web - {project?.images?.web.length} imagens
+            </h2>
+          </Platform>
+
+          <Carousel web>
+            <Slider {...settingsWeb}>
+              {project?.images!.web.map((image) => (
+                <div key={image.id} className="project-carousel-content">
+                  <img
+                    className="project-carousel-image-web"
+                    src={`/projects/${project.title}/web/${image.pathName}.png`}
+                    alt={image.pathName}
+                  />
+                </div>
+              ))}
+            </Slider>
+          </Carousel>
+        </>
+      ) : null}
+
+      {projects[parseInt(params.id) - 1].platform.some(
+        (item) => item.title === 'mobile'
+      ) ? (
+        <>
+          <Platform mobile>
+            <h2 className="project-platform-title">
+              mobile - {project?.images?.mobile.length} imagens
+            </h2>
+          </Platform>
+
+          <Carousel mobile>
+            <Slider {...settingsMobile}>
+              {project?.images!.mobile.map((image) => (
+                <div key={image.id} className="project-carousel-content">
+                  <img
+                    className="project-carousel-image-mobile"
+                    src={`/projects/${project.title}/mobile/${image.pathName}.png`}
+                    alt={image.pathName}
+                  />
+                </div>
+              ))}
+            </Slider>
+          </Carousel>
+        </>
+      ) : null}
+
+      <Separator />
+
+      <h2 className="project-subtitle">Sobre</h2>
+      <p className="project-description">{project?.description}</p>
+
+      <Separator />
+
+      <h2 className="project-subtitle">Tecnologias utilizadas</h2>
+      <div className="project-techs">
+        {project?.stacks.map((stack) => (
+          <StackTag key={stack.id} title={stack.title} color={stack.color} />
+        ))}
       </div>
+
+      <Separator />
+
+      <h2 className="project-subtitle">Protótipo</h2>
+      <a
+        className="project-link"
+        href={project?.figma}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {project?.title} - Figma
+      </a>
+
+      <Separator />
+
+      <h2 className="project-subtitle">Código da aplicação</h2>
+      <a
+        className="project-link"
+        href={project?.github}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {project?.title} - GitHub
+      </a>
+
+      <Footer />
     </ProjectContainer>
   );
 };
--- web/src/pages/Project/styles.ts
+++ web/src/pages/Project/styles.ts
@@ -1,17 +1,111 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+interface Props {
+  web?: boolean;
+  mobile?: boolean;
+}
 
 const ProjectContainer = styled.div`
-  width: 70%;
-
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  padding: 6rem 13rem 18rem;
 
   position: relative;
 
-  .project-carousel-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  .project-title {
+    background: linear-gradient(150deg, #ffffff 2%, #39393a 80%);
+    -webkit-text-fill-color: transparent;
+    -webkit-background-clip: text;
+    align-self: flex-start;
+    font-size: 4rem;
+  }
+
+  .project-description {
+    font-size: 2.4rem;
+    margin: 0 1.4rem 3rem;
+    text-align: justify;
+  }
+
+  .project-subtitle {
+    align-self: flex-start;
+    font-size: 3rem;
+    margin-bottom: 2rem;
+  }
+
+  .project-techs {
+    width: 100%;
+    margin-bottom: 3rem;
+
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+  }
+
+  .project-link {
+    align-self: flex-start;
+    margin: 0 2rem 3rem;
+    font-size: 2.4rem;
+    color: #e6e6e6;
+  }
+
+  @media screen and (max-width: 767px) {
+    padding: 3rem 2rem 12rem;
+
+    .project-title {
+      font-size: 3rem;
+    }
+
+    .project-subtitle {
+      font-size: 2.6rem;
+    }
+
+    .project-description,
+    .project-link {
+      font-size: 2.2rem;
+    }
+
+    .project-techs {
+      flex-wrap: wrap;
+    }
+  }
+`;
+
+const Platform = styled.div<Props>`
+  align-self: flex-start;
+  border-radius: 2rem;
+  margin: 3rem 0 3rem 5rem;
+  padding: 0.5rem 1rem;
+
+  ${(props) =>
+    props.web &&
+    css`
+      background-color: #61dafb;
+    `}
+
+  ${(props) =>
+    props.mobile &&
+    css`
+      background-color: #9b79fc;
+    `}
+  
+  @media screen and (max-width: 767px) {
+    margin-left: 2rem;
+  }
+`;
+
+const Carousel = styled.div<Props>`
+  width: 70rem;
+  margin: 5rem 0;
+
+  .project-carousel-image-web {
     width: 50rem;
-    margin: 5rem 0;
+  }
+
+  .project-carousel-image-mobile {
+    height: 50rem;
   }
 
   .slick-slide {
@@ -20,26 +114,53 @@ const ProjectContainer = styled.div`
     justify-content: center;
   }
 
-  .slick-center {
-    -webkit-transform: scale(1.25);
-    -moz-transform: scale(1.25);
-    transform: scale(1.25);
+  .slick-dots {
+    position: relative;
   }
 
-  .project-carousel-content {
-    
+  .slick-dots li {
+    width: 1rem;
+    height: 1rem;
   }
 
-  .project-carousel-image {
-    width: 25rem;
-    /* transition: transform .2s; */
+  .slick-dots .slick-active {
+    width: 1rem;
+    height: 1rem;
+    border-radius: 1rem;
   }
 
-  .project-carousel-image:hover {
-    /* transform: scale(1.5);
-    z-index: 1000; */
-    width: 50rem;
+  .slick-prev:before,
+  .slick-next:before {
+    content: '';
+  }
+
+  ${(props) =>
+    props.web &&
+    css`
+      .slick-dots .slick-active {
+        background-color: #61dafb;
+      }
+    `}
+
+  ${(props) =>
+    props.mobile &&
+    css`
+      .slick-dots .slick-active {
+        background-color: #9b79fc;
+      }
+    `}
+
+  @media screen and (max-width: 767px) {
+    width: 80%;
+
+    .project-carousel-image-web {
+      width: 23rem;
+    }
+
+    .project-carousel-image-mobile {
+      height: 38rem;
+    }
   }
 `;
 
-export { ProjectContainer };
+export { ProjectContainer, Platform, Carousel };
