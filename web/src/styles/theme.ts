29f6401b342492b3a13e400c38430a8be4cad46c
Fellipe Araujo <araujo.fellipe@hotmail.com>
2022-02-03 22:52:47 -0300

    Created and configured theme for the project.

---
+++ web/src/styles/theme.ts
@@ -0,0 +1,10 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  colors: {
+    background_primary: '#121214',
+    background_secondary: '#202024',
+    main: '#00B37E',
+    text: '#ffffff',
+    text_detail: '#39393a',
+  },
+};
